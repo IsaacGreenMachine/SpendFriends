7be82fb95ee6af6d0bee462da3528e67d6ba13de
Isaac Green <zuderg@gmail.com>
2022-01-29 12:11:42 -0600

    first functioning prototype

--- app.py
+++ app.py
@@ -3,9 +3,13 @@
 this script creates a page to return hello hbnb
 '''
 from urllib import request
-from flask import Flask, render_template, request, redirect
+import uuid
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 import json
+from uuid import uuid4
+
+from matplotlib.font_manager import json_dump
 app = Flask(__name__)
 con = sqlite3.connect('example.db', check_same_thread=False)
 cur = con.cursor()
@@ -23,7 +27,7 @@ def loginPage():
 def loginPagePost():
     '''This method creates a route and gives it some text'''
     if cur.execute('SELECT * FROM users WHERE username = "{}"'.format(request.form['username'])).fetchall() == []:
-        return "Invalid Username!"
+        return redirect('/loginPage/createUser/{}/{}'.format(request.form['username'], request.form['password']))
 
     else:
         if cur.execute('SELECT * FROM users WHERE username = "{}" AND password = "{}"'.format(request.form['username'], request.form['password'])).fetchall() == []:
@@ -54,8 +58,22 @@ def addExpense(username):
 
 @app.route('/overview/<username>/addExpense', methods=['POST'], strict_slashes=False)
 def addExpensePost(username):
-    #cur.execute("UPDATE users SET incomes = incomes || '{}' WHERE user_id = '{}'".format([], username))
-    #request.form[''] #expName expAmt expCat
+    expenses = json.loads(cur.execute('SELECT expenses FROM users WHERE username = "{}"'.format(username)).fetchall()[0][0])
+    expenses.append([request.form['expName'], round(float(request.form['expAmt'].replace(',', '')), 2), request.form['expCat']])
+    cur.execute("UPDATE users SET expenses = '{}' WHERE username = '{}'".format(json.dumps(expenses), username))
+    con.commit()
+    return redirect('/overview/{}'.format(username))
+
+@app.route('/overview/<username>/addIncome', strict_slashes=False)
+def addIncome(username):
+    return render_template('addIncome.html')
+
+@app.route('/overview/<username>/addIncome', methods=['POST'], strict_slashes=False)
+def addIncomePost(username):
+    incomes = json.loads(cur.execute('SELECT incomes FROM users WHERE username = "{}"'.format(username)).fetchall()[0][0])
+    incomes.append([request.form['incName'], round(float(request.form['incAmt'].replace(',', '')), 2), request.form['incCat']])
+    cur.execute("UPDATE users SET incomes = '{}' WHERE username = '{}'".format(json.dumps(incomes), username))
+    con.commit()
     return redirect('/overview/{}'.format(username))
 
 @app.route('/friends', strict_slashes=False)
@@ -87,6 +105,20 @@ def displayUsers():
     '''displays info from SQLite about user'''
     users = cur.execute('SELECT * FROM users').fetchall()
     return render_template("users.html", users=users)
+
+@app.route('/loginPage/createUser/<username>/<password>', strict_slashes=False)
+def createUser(username, password):
+    '''displays info from SQLite about user'''
+    return render_template("createUser.html", username=username, password=password)
+
+@app.route('/loginPage/createUser/<username>/<password>', methods=['POST'], strict_slashes=False)
+def createUserPost(username, password):
+    '''displays info from SQLite about user'''
+    cur.execute("INSERT INTO users VALUES ('{}','{}', '{}', '', '{}', '{}', '')".format(username, password, uuid.uuid4(), json.dumps([]), json.dumps([])))
+    con.commit()
+    return redirect('/overview/{}'.format(username))
+
+
 
 
 if __name__ == '__main__':
